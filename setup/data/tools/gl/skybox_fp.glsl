bf560826e1d0c2dd6859e5798bc615cc363ecd46
Garux <garux@mail.ru>
2024-10-31 03:59:11 +0500

    deldeldel

--- setup/data/tools/gl/skybox_fp.glsl
+++
@@ -1,8 +0,0 @@
-
-uniform samplerCube skybox;
-
-void main()
-{
-	//doing rotation/flip to fix skybox orientation
-	gl_FragColor = textureCube( skybox, vec3( -gl_TexCoord[0].y, gl_TexCoord[0].z, gl_TexCoord[0].x ) );
-}
